f547ae048155f365ecab7084ef4773dc03510239
Michal <noszczykmichal@wp.pl>
2022-10-14 01:36:20 +0200

    disabling the TrashIcon when there's not favorite meetups

--- src/components/ui/TrashIcon.js
+++ src/components/ui/TrashIcon.js
@@ -1,21 +1,37 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
 import PropTypes from "prop-types";
+import { useContext } from "react";
+
 import classes from "./TrashIcon.module.css";
+import FavoritesContext from "../../store/favorites-context";
 
 function TrashIcon({ clicked }) {
+  const { totalFavorites } = useContext(FavoritesContext);
+  let attachedClasses = [classes.container];
+
+  if (!totalFavorites) {
+    attachedClasses = [classes.container, classes["container--inactive"]];
+  }
+
   return (
-    <div className={classes.badge} onClick={clicked}>
+    <button
+      type="button"
+      className={attachedClasses.join(" ")}
+      onClick={clicked}
+      disabled={!totalFavorites}
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
         fillRule="evenodd"
         clipRule="evenodd"
-        className={classes["badge__trash-icon"]}
-        alt="delete favorites meetups"
+        className={classes.container__icon}
+        alt="delete favorite meetups"
       >
         <path d="M19 24h-14c-1.104 0-2-.896-2-2v-16h18v16c0 1.104-.896 2-2 2zm-7-10.414l3.293-3.293 1.414 1.414-3.293 3.293 3.293 3.293-1.414 1.414-3.293-3.293-3.293 3.293-1.414-1.414 3.293-3.293-3.293-3.293 1.414-1.414 3.293 3.293zm10-8.586h-20v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2zm-8-3h-4v1h4v-1z" />
       </svg>
-    </div>
+    </button>
   );
 }
 
